88e6de8eaae6430cc367f11effcbd45d7294b9fa
Tommyk <tomm8231@stud.kea.dk>
2023-11-16 13:07:32 +0100

    database stuff added

---
+++ server/databases/connections.js
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+await client.connect();
+
+const dbName = "staff_orders";
+const db = await client.db(dbName);
+
+export default {
+    items: db.collection("S2S")
+};
