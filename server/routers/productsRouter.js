f4a5baa3d7f322850a495eb4e6a5ca9bd16cf803
Tommyk <tomm8231@stud.kea.dk>
2023-11-20 11:12:43 +0100

    refactoring

--- server/routers/productsRouter.js
+++ server/routers/productsRouter.js
@@ -4,11 +4,11 @@ const router = Router();
 import { getProductsCollection } from "../databases/read.js";
 
 router.get("/products/:round", async (req, res) => {
-    const round = req.params?.round
+    const round = req.params?.round;
     const productsCollection = getProductsCollection;
     const allProducts = await productsCollection.find({ round }).toArray();
 
     res.send({ data: allProducts });
-})
+});
 
 export default router;
