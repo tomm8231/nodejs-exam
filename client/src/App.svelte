90ecee44797a73dfcd1a028a4b3fe6927ed69378
SebastianHeiberg <seba3673@stud.kea.dk>
2023-11-24 11:30:26 +0100

    new user start

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,6 +3,7 @@
   import ShowItems from './pages/ShowItems/ShowItems.svelte';
   import Home from "./pages/Home.svelte";
   import ShowItemsAdmin from './pages/ShowItemsAdmin/ShowItemsAdmin.svelte';
+  import AddUser from './pages/addUser/AddUser.svelte';
 </script>
 
 <Router>
@@ -10,11 +11,13 @@
     <Link to="/">Home</Link>
     <Link to="/ShowItems">Show items</Link>
     <Link to="/ShowItemsAdmin">Show items (Admin)</Link>
+    <Link to="/adduser">Add user (Admin)</Link>
   </nav>
   <div>
     <Route path="/" component={Home}/>
     <Route path="/ShowItems" component={ShowItems}/>
     <Route path="/ShowItemsAdmin" component={ShowItemsAdmin}/>
+    <Route path="/adduser" component={AddUser}/>
   </div>
 
 </Router>
