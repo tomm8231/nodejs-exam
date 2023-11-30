f93d41732ff4a58e44a9f733c61be06a45924b93
SebastianHeiberg <seba3673@stud.kea.dk>
2023-11-30 09:50:59 +0100

    nav fix

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,59 +1,66 @@
 <script>
-  import {Router, Link, Route, navigate} from 'svelte-navigator';
-  import PrivateRoute from './privateRouting/PrivateRoute.svelte';
-  import ShowItems from './pages/ShowItems/ShowItems.svelte';
+  import { Router, Link, Route, navigate } from "svelte-navigator";
+  import PrivateRoute from "./privateRouting/PrivateRoute.svelte";
+  import ShowItems from "./pages/ShowItems/ShowItems.svelte";
   import Home from "./pages/Home.svelte";
-  import ShowItemsAdmin from './pages/ShowItemsAdmin/ShowItemsAdmin.svelte';
-  import AddUser from './pages/AddUser/AddUser.svelte';
-  import Login from "./pages/Login/Login.svelte"
-  import ShowUsers from './pages/showUsers/ShowUsers.svelte';
-  import { user } from './stores/userStore.js'
-  import ShowOrder from './pages/ShowOrderAdmin/ShowOrder.svelte';  
-  import { onMount } from 'svelte';
+  import ShowItemsAdmin from "./pages/ShowItemsAdmin/ShowItemsAdmin.svelte";
+  import AddUser from "./pages/AddUser/AddUser.svelte";
+  import Login from "./pages/Login/Login.svelte";
+  import ShowUsers from "./pages/showUsers/ShowUsers.svelte";
+  import { user } from "./stores/userStore.js";
+  import ShowOrder from "./pages/ShowOrderAdmin/ShowOrder.svelte";
+  import { onMount } from "svelte";
 
   onMount(async () => {
-
-    if (localStorage.getItem("currentUserId") && localStorage.getItem("currentUserRole")) {
-      const uid = localStorage.getItem("currentUserId")
-      const role =  localStorage.getItem("currentUserRole")
-      $user =  {uid, role}
+    if (
+      localStorage.getItem("currentUserId") &&
+      localStorage.getItem("currentUserRole")
+    ) {
+      const uid = localStorage.getItem("currentUserId");
+      const role = localStorage.getItem("currentUserRole");
+      $user = { uid, role };
     }
-
-  
-  })
+  });
 
   async function logout() {
     // Perform logout logic here
     // For example, clear user data from localStorage and navigate to the login page
     localStorage.removeItem("currentUserId");
     localStorage.removeItem("currentUserRole");
-    navigate('/login');
+    navigate("/login");
   }
-
 </script>
 
 <Router>
   <div id="app">
-  {#if $user}
-  <nav>
-    {#if $user}
-    <Link to="/">Home</Link>
-    <Link to="/ShowItems">Show items</Link>
-    <Link to="/ShowItemsAdmin">Show items (Admin)</Link>
-    <Link to="/adduser">Tilføj bruger (Admin)</Link>
-    <Link to="/showusers">Vis brugere (Admin)</Link>
-    <Link to="/showorderadmin">Vis ordre (Admin)</Link>
-    <a href="/" on:click={logout}>Log ud</a>
+    {#if $user && $user.role === "ADMIN"}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/ShowItemsAdmin">Show items (Admin)</Link>
+        <Link to="/adduser">Tilføj bruger (Admin)</Link>
+        <Link to="/showusers">Vis brugere (Admin)</Link>
+        <Link to="/showorderadmin">Vis ordre (Admin)</Link>
+        <a href="/" on:click={logout}>Log ud</a>
+      </nav>
+    {:else if $user && $user.role === "USER"}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/ShowItems">Show items</Link>
+        <a href="/" on:click={logout}>Log ud</a>
+      </nav>
     {/if}
-  <div class="content">
-    <PrivateRoute path="/" let:location><Home /></PrivateRoute>
-    <PrivateRoute path="/ShowItems" let:location><ShowItems /></PrivateRoute>
-    <PrivateRoute path="/ShowItemsAdmin" let:location><ShowItemsAdmin /></PrivateRoute>
-    <PrivateRoute path="/adduser" let:location><AddUser /></PrivateRoute>
-    <PrivateRoute path="/showusers" let:location><ShowUsers /></PrivateRoute>
-    <PrivateRoute path="/showorderadmin" let:location><ShowOrder /></PrivateRoute>
-    <Route path="/login"><Login /></Route>
+    <div class="content">
+      <PrivateRoute path="/" let:location><Home /></PrivateRoute>
+      <PrivateRoute path="/ShowItems" let:location><ShowItems /></PrivateRoute>
+      <PrivateRoute path="/ShowItemsAdmin" let:location
+        ><ShowItemsAdmin /></PrivateRoute
+      >
+      <PrivateRoute path="/adduser" let:location><AddUser /></PrivateRoute>
+      <PrivateRoute path="/showusers" let:location><ShowUsers /></PrivateRoute>
+      <PrivateRoute path="/showorderadmin" let:location
+        ><ShowOrder /></PrivateRoute
+      >
+      <Route path="/login"><Login /></Route>
+    </div>
   </div>
-</div>
 </Router>
-
