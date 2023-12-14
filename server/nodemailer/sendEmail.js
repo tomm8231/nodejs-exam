7b9338ba411cce7b81ddae2d63af21f3877b4209
SebastianHeiberg <seba3673@stud.kea.dk>
2023-12-14 10:13:52 +0100

    can send a mail

---
+++ server/nodemailer/sendEmail.js
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+import nodemailer from 'nodemailer'
+
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: "tommyhellejensen@gmail.com",
+    subject: "Hello from Nodemailer",
+    text: "This is a test email sent using Nodemailer.",
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
+  });
