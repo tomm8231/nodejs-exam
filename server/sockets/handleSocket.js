b4cb286d660630df80b3ea6dcba71a92ff1a41be
Tommyk <tomm8231@stud.kea.dk>
2023-12-14 11:19:19 +0100

    email notification works

--- server/sockets/handleSocket.js
+++ server/sockets/handleSocket.js
@@ -1,4 +1,5 @@
 import db from "../databases/connections.js";
+import { sendMail } from "../nodemailer/sendEmail.js";
 
 function handleSocket(socket) {
 
@@ -9,6 +10,16 @@ function handleSocket(socket) {
     socket.on("client-admin-message", async (data) => {
         data.date = new Date();
         await db.collection("messages").insertOne(data);
+        const roundOrders = await db.collection("orders").find({ round: data.round, staffNumber: { $exists: true } }).toArray()
+        const staffNumbersFromRound = roundOrders.map(order => order.staffNumber);
+        const roundUsers = await db.collection("users").find({
+            staffNumber: { $in: staffNumbersFromRound }
+        }).toArray();
+        const emails = roundUsers.map(user => user.email);
+
+        const message = `Der er kommet en ny besked vedr. bestillingsrunde ${data.round}: ${data.message}`
+
+        sendMail(emails, `Vedr. ${data.round}`, message)
         socket.broadcast.emit("server-admin-message", data);
     });
 }
