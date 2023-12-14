0277b97676693117ac8aff69c550f25dce671e72
SebastianHeiberg <seba3673@stud.kea.dk>
2023-12-14 10:19:40 +0100

    send mail function works

--- server/nodemailer/sendEmail.js
+++ server/nodemailer/sendEmail.js
@@ -1,29 +1,35 @@
-import 'dotenv/config'
-import nodemailer from 'nodemailer'
-
+import "dotenv/config";
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
+export async function sendMail(recipient, subject, message) {
   const mailOptions = {
     from: process.env.EMAIL,
-    to: "tommyhellejensen@gmail.com",
-    subject: "Hello from Nodemailer",
-    text: "This is a test email sent using Nodemailer.",
+    to: recipient,
+    subject: subject,
+    text: message,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email: ", error);
-    } else {
-      console.log("Email sent: ", info.response);
-    }
-  });
+  try {
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+      } else {
+        console.log("Email sent: ", info.response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
